46384e11a17158d61ed718f2b6660493c3c1a2a5
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-11-17 18:59:05 +0100

    Scaffolding: add components. install bootstrap sass

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script>
 import {store} from "./store";
+import AppHeader from "./components/AppHeader.vue"
+import AppJumboForm from "./components/AppJumboForm.vue"
+import AppSections from "./components/AppSections.vue"
+import AppSubscriber from "./components/AppSubscriber.vue"
+import AppFooter from "./components/AppFooter.vue"
+
 export default {
   components: {
+    AppHeader,
+    AppJumboForm,
+    AppSections,
+    AppSubscriber,
+    AppFooter
 
   },
   data(){
@@ -13,6 +24,26 @@ export default {
 </script>
 
 <template>
+
+  <!-- header -->
+  <AppHeader />
+  <!-- /header -->
+
+  <!-- jumbo-form -->
+  <AppJumboForm />
+  <!-- /jumbo-form -->
+
+  <!-- sections -->
+  <AppSections/>
+  <!-- /sections -->
+
+  <!-- subscribe form -->
+  <AppSubscriber />
+  <!-- /subscribe form -->
+
+  <!-- footer -->
+  <AppFooter/>
+  <!-- footer -->
 
 </template>
 
