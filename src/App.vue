25fad0a8ffd0fec4a721128107bba7350cb26527
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-11-17 18:14:34 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import {store} from "./store";
+export default {
+  components: {
+
+  },
+  data(){
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *
+
+</style>
